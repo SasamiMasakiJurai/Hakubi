50bde553c2dff081bf92dd13d16b686bf032aa9d
Sasami Masaki Jurai <37247512+SasamiMasakiJurai@users.noreply.github.com>
2021-11-16 10:48:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,5 +12,6 @@
 
 # Add a feed source
 git clone https://github.com/yichya/luci-app-xray package/luci-app-xray
+git clone https://github.com/yichya/openwrt-xray package/openwrt-xray
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
